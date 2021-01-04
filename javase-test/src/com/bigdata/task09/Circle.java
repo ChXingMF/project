c95872095a0222a9ba05f7f7fc5fbd6deaba8533
ChXingMF <18651693981@163.com>
2021-01-04 23:03:27 +0800

    2021/01/04

--- javase-test/src/com/bigdata/task09/Circle.java
+++ javase-test/src/com/bigdata/task09/Circle.java
@@ -1,4 +1,31 @@
 package com.bigdata.task09;
 
 public class Circle extends Shape {
+    private int ir;
+
+    public Circle() {
+    }
+
+    public Circle(int x, int y, int ir) {
+        super(x, y);
+        setIr(ir);
+    }
+
+    public int getIr() {
+        return ir;
+    }
+
+    public void setIr(int ir) {
+        if (ir > 0){
+            this.ir = ir;
+        }else {
+            System.out.println("radius is illegal!");
+        }
+    }
+
+    @Override
+    public void show() {
+        super.show();
+        System.out.println("radus is " + getIr());
+    }
 }
--- javase-test/src/com/bigdata/task09/ShapeTest.java
+++ javase-test/src/com/bigdata/task09/ShapeTest.java
@@ -3,10 +3,21 @@ package com.bigdata.task09;
 public class ShapeTest {
 
     // 自定义成员方法实现将参数指定矩形对象特征打印出来的行为，也就是绘制图形的行为
-    public static  void draw(Rect rt) {
-        
+//    public static  void draw(Rect rt) {
+//        rt.show();
+//    }
+
+    // 自定义成员方法实现将参数指定圆形对象特征打印出来的行为
+//    public static void draw(Circle c) {
+//        c.show();
+//    }
+
+    // 自定义成员方法实现既能打印矩形对象又能打印圆形对象的特征，对象由参数传入  子类 is a 父类
+    public  static void draw(Shape s) {
+        s.show();
     }
     public static void main(String[] args) {
-
+        ShapeTest.draw(new Rect(1, 2, 3,4));
+        ShapeTest.draw(new Circle(5, 6, 7));
     }
 }
